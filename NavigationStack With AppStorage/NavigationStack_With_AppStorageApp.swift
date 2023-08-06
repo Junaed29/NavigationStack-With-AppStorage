c909295ee2c6207b5a6e40c0096e5d85948eef02
Junaed <junaed1995@gmail.com>
2023-08-06 14:40:54 +0600

    Initial Commit

---
+++ NavigationStack With AppStorage/NavigationStack_With_AppStorageApp.swift
@@ -0,0 +1,17 @@
+//
+//  NavigationStack_With_AppStorageApp.swift
+//  NavigationStack With AppStorage
+//
+//  Created by Junaed Muhammad Chowdhury on 6/8/23.
+//
+
+import SwiftUI
+
+@main
+struct NavigationStack_With_AppStorageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
